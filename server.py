1298daf01a1d8bf84b8f7dcb94523df9701c63b8
Andrea Pullen <apullenb@gmail.com>
2023-04-10 13:34:25 -0500

    add templates and routes

--- server.py
+++ server.py
@@ -6,10 +6,19 @@ app.secret_key = 'secret123'
 
 
 
-
 @app.route("/", methods=['GET'])
 def success():
-    return '<h1>Welcome!</h1>'
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=["GET", "POST"])
+def process_submission():
+    session['name'] = request.form['name']
+    session['dojo_location'] = request.form['dojo_location']
+    session['favorite_language'] = request.form['favorite_language']
+    session['comment'] = request.form['comment']
+    return render_template('result.html')
+
     
 @app.route('/destroy_session')
 def reset_session():
